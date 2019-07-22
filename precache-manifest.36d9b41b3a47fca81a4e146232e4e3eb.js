fc355a3e1e6faafe6c213536ca18d1d3ce43cf57
Facundo Petre <facu1p3@gmail.com>
2019-07-22 00:12:49 -0300

    Updates

---
+++ precache-manifest.36d9b41b3a47fca81a4e146232e4e3eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257dc905243051ce35b6a07d1a15b7d5",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "b62fe154303035eaffc4",
+    "url": "/calendar/static/css/2.ddd4d984.chunk.css"
+  },
+  {
+    "revision": "0d85f07e8ea98826bd43",
+    "url": "/calendar/static/css/main.54ce179b.chunk.css"
+  },
+  {
+    "revision": "b62fe154303035eaffc4",
+    "url": "/calendar/static/js/2.d354a884.chunk.js"
+  },
+  {
+    "revision": "0d85f07e8ea98826bd43",
+    "url": "/calendar/static/js/main.34c29175.chunk.js"
+  },
+  {
+    "revision": "70cd18d5f6522f88cc6f",
+    "url": "/calendar/static/js/runtime~main.3b46a14a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/precache-manifest.821f313804123f7f3fb664b1747fcac6.js"
+  "/calendar/precache-manifest.36d9b41b3a47fca81a4e146232e4e3eb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/calendar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
