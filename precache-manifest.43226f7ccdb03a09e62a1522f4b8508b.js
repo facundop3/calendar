7adf329b62d69c8cd65187baa537e44734d64cb2
Facundo Petre <facu1p3@gmail.com>
2019-07-22 00:02:11 -0300

    Updates

---
+++ precache-manifest.43226f7ccdb03a09e62a1522f4b8508b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf0bb15f65c7ee52758ddece7efc2a1",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "9ce33d8cc4c715862472",
+    "url": "/my-app/static/css/2.ddd4d984.chunk.css"
+  },
+  {
+    "revision": "a218f70796781e440a23",
+    "url": "/my-app/static/css/main.54ce179b.chunk.css"
+  },
+  {
+    "revision": "9ce33d8cc4c715862472",
+    "url": "/my-app/static/js/2.ef7aecbd.chunk.js"
+  },
+  {
+    "revision": "a218f70796781e440a23",
+    "url": "/my-app/static/js/main.ab253cef.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  }
+]);
