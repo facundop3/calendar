4e47901f7484a2ec1235c1fc0b207ee7dc555aa7
facundop3 <fpetre@altimetrik.com>
2019-07-29 10:09:41 -0300

    Updates

---
+++ precache-manifest.b045dde24ecb2f20d72576505f1bd977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fb580641de1f7e666c7b0b1cd2608f3",
+    "url": "/fpetre/index.html"
+  },
+  {
+    "revision": "f433c8e60dd9b791c6dd",
+    "url": "/fpetre/static/css/2.ddd4d984.chunk.css"
+  },
+  {
+    "revision": "cd5a4c45b047294d1c4f",
+    "url": "/fpetre/static/css/main.fd1d1684.chunk.css"
+  },
+  {
+    "revision": "f433c8e60dd9b791c6dd",
+    "url": "/fpetre/static/js/2.c011e6af.chunk.js"
+  },
+  {
+    "revision": "cd5a4c45b047294d1c4f",
+    "url": "/fpetre/static/js/main.41d757fa.chunk.js"
+  },
+  {
+    "revision": "ebe674c2c398b93d6529",
+    "url": "/fpetre/static/js/runtime~main.ca870218.js"
+  }
+]);
