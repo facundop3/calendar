3e2954363f73642688f8ae5e61833bd9d0d708d1
Facundo Petre <fpetre@paypal.com>
2021-01-26 00:59:28 -0300

    Updates

---
+++ precache-manifest.c97c36b9470f9f40d6f6cb4aa93d3e77.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "082ce44f81bdbed3e35fff589268c447",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "032b8b2e8addfb928c47",
+    "url": "/calendar/static/css/2.55aa8cdb.chunk.css"
+  },
+  {
+    "revision": "f4fefc95add6367e5e16",
+    "url": "/calendar/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "032b8b2e8addfb928c47",
+    "url": "/calendar/static/js/2.d6e97d84.chunk.js"
+  },
+  {
+    "revision": "f4fefc95add6367e5e16",
+    "url": "/calendar/static/js/main.a3bb0269.chunk.js"
+  },
+  {
+    "revision": "70cd18d5f6522f88cc6f",
+    "url": "/calendar/static/js/runtime~main.3b46a14a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/calendar/precache-manifest.5b5c5fc6df3d777b902c7554f9a17223.js"
+  "/calendar/precache-manifest.c97c36b9470f9f40d6f6cb4aa93d3e77.js"
 );
 
 self.addEventListener('message', (event) => {
